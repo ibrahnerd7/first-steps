9ee86ebb447e845dd35c8d51c028f53137a6ecf3
Ibrahim Maina <ibrahim.maina@managinglife.com>
2025-07-16 13:07:08 +0300

    Query parameters

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from typing import  Union
-from fastapi import  FastAPI
-from pydantic import BaseModel
+from typing import  Union, Annotated
+from fastapi import  FastAPI, Query
+from pydantic import BaseModel, AfterValidator
 from enum import Enum
 
 from pygments.lexers import q
@@ -85,3 +85,30 @@ async def update_item(item_id:int, item:Item2, q:str |None = None):
     if q:
         result.update({"q": q})
     return result
+
+@app.get("/items3/")
+async def read_items(q: Annotated[str |None, Query(min_length=3,max_length=50, pattern="^fixedquery$")] ):
+    results = [{"items": "Foo"}, {"item_id": "Bar"}]
+    if q:
+        results.update({"q": q})
+    return results
+
+data = {
+    "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
+    "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
+    "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",
+}
+def check_is_valid_id(id: str):
+    if not id.startswith("isbn", "imdb-"):
+        raise ValueError('Invalid ID format, it must start with "isbn-" or "imdb-"')
+    return id
+
+@app.get("/books")
+async def read_books(
+        id: Annotated[str | None,AfterValidator(check_is_valid_id)] = None,
+):
+    if id:
+        item = data.get(id)
+    else:
+        id, item = random.choice(list(data.items()))
+    return {"id": id, "item": item}
