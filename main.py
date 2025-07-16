487ca63d738d2c98482318c47d7666bc7a17987e
Ibrahim Maina <ibrahim.maina@managinglife.com>
2025-07-16 12:25:17 +0300

    Query params

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ async def get_model(model_name: ModelName):
     return {"model_name": model_name , "message": "Have some residuals"}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q:Union[int, str] = None):
+def read_item(item_id: int, q:str | None = None):
     return {"item_id": item_id, "q": q}
 
 @app.put("/items/{item_id}")
@@ -38,3 +38,27 @@ def update_item(item_id: int, item:Item):
 @app.get("/files/{file_path}")
 def read_file(file_path: str):
     return {"file_path": file_path}
+
+fake_items_db =[
+    {
+        "item_name": "Foo"
+    },
+    {
+        "item_name": "Bar"
+    },
+    {
+        "item_name": "Baz"
+    }
+]
+@app.get("/items/")
+async def read_item(skip:int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q:str = None, short: bool = False):
+    item  = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This is an amazing item that has a long description"})
+    return item
